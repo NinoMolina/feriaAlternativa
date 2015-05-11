9750c86f318f490c1afc55dbe2a7fbcf7fe8bd30
nino molina <leandro.e.molina@gmail.com>
2015-05-11 16:10:47 -0300

    Revert "modified facebook page refresh"
    This reverts commit c122918284edc8b8626fdbe4b098415e0365826f.

--- app/facebook/facebookController.js
+++ app/facebook/facebookController.js
@@ -2,16 +2,13 @@
 
 angular.module('facebook', [])
 .controller('FacebookCtrl', ['$scope',function($scope) {
-        //$scope.template = {url: '/facebook/facebook.html?r=' + window.Math.random()};
         $scope.template = {url: '/facebook/facebook.html'};
-        loadFacebookSdk(document, 'script', 'facebook-jssdk');
 }]);
 
-var loadFacebookSdk = function(d, s, id) {
+(function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
         js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.3&appId=174931712664653";
         fjs.parentNode.insertBefore(js, fjs);
-};
-
+}(document, 'script', 'facebook-jssdk'));
